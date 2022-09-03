38447bcd7cb655df80005109636984279173dec4
Olivier Huin <olih@users.noreply.github.com>
2022-09-03 17:37:43 +0100

    remove dependency with fs-jetpack

--- src/testing-io.ts
+++ src/testing-io.ts
@@ -1,5 +1,4 @@
 import YAML from 'yaml';
-import jetpack from 'fs-jetpack';
 import { readFile } from 'node:fs/promises';
 import {
   FileParser,
@@ -14,10 +13,6 @@ export const getZestYaml = async (
   const content = await readFile(filename, { encoding: 'utf8' });
   const contentObject = YAML.parse(content);
   return safeParseTestingModel(contentObject);
-};
-
-export const findZestFiles = async (parentDir: string): Promise<string[]> => {
-  return await jetpack.findAsync(parentDir, { matching: '**/*.zest.yaml' });
 };
 
 // TODO: Pick up parser as config
