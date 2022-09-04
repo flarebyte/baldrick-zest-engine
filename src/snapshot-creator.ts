4f5e41e671619e2ff13914396d0be2989fc30ea7
Olivier Huin <olih@users.noreply.github.com>
2022-09-04 19:00:30 +0100

    Draft snapshot

--- src/snapshot-creator.ts
+++ src/snapshot-creator.ts
@@ -1,7 +1,26 @@
-import { TestCaseResult } from './execution-context-model.js';
+import path from 'node:path';
+import { TestCaseExecutionContext } from './execution-context-model.js';
+import { writeSnapshotFile } from './testing-io.js';
+import {
+  FileParser,
+  TestingFunctionTestCaseModel,
+  TestingModel,
+} from './testing-model.js';
 
-const checkSnapshot = (
-    testCaseResult: TestCaseResult
+export const getSnapshotFilename = (
+  testingModel: TestingModel,
+  _testCase: TestingFunctionTestCaseModel
+): string => {
+  return path.join(testingModel.snapshotDir, 'temp.yaml');
+};
+
+const checkSnapshot = async (
+  executionContext: TestCaseExecutionContext,
+  snapshotFileName: string,
+  parser: FileParser,
+  actual: string | object
 ) => {
-    
+  if (executionContext.expected === undefined) {
+    await writeSnapshotFile(snapshotFileName, actual, { parser });
+  }
 };
