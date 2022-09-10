e0db4b9ac823124a467fa72501593a4414fa7c80
Olivier Huin <olih@users.noreply.github.com>
2022-09-10 16:55:45 +0100

    More example of transformers

--- spec/example-transformer.ts
+++ spec/example-transformer.ts
@@ -7,14 +7,19 @@ export const addPrefix2 = (prefix: string, value: string) =>
 export const addPrefix3 = (prefix: string) => (value: string) =>
   `${prefix}${value}`;
 
-  export class PrefixA {
-    static addPrefix4(prefix: string, value: string): string {
-      return `${prefix}${value}`;
-    }
-    static addPrefix5(value: string, prefix: string): string {
-      return `${prefix}${value}`;
-    }
+export const makeUpperCase = (value: string) => value.toUpperCase();
+
+export class PrefixA {
+  static addPrefix4(prefix: string, value: string): string {
+    return `${prefix}${value}`;
   }
+  static addPrefix5(value: string, prefix: string): string {
+    return `${prefix}${value}`;
+  }
+  static makeUpperCase6(value: string) {
+    value.toUpperCase();
+  }
+}
 export class PrefixB {
   prefix: string;
   constructor(prefix: string) {
@@ -22,5 +27,15 @@ export class PrefixB {
   }
   withPrefix(value: string) {
     return `${this.prefix}${value}`;
+  }
+}
+
+export class ValueHolderC {
+  value: string;
+  constructor(value: string) {
+    this.value = value;
+  }
+  toUpper() {
+    return this.value.toUpperCase();
   }
 }
